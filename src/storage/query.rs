fd2284da407438ae838242b91abadf707af0536c
Harry Prior <harryprior@hotmail.co.uk>
2023-01-07 14:40:09 +0000

    still very much wip, need to implement an iterator over generic tuples somehow

--- src/storage/query.rs
+++ src/storage/query.rs
@@ -1,4 +1,5 @@
 use std::marker::PhantomData;
+use std::path::Iter;
 use crate::entity;
 use crate::storage::component::TypeInfo;
 
@@ -8,6 +9,77 @@ use super::{component::Component, table::EntityTable};
 // In a real case several tables would be queries for matching types, and a 2d array would be returned
 // create an abstraction that would allow for the iterating over multiple arrays of a particular type
 // and for tuples of various types
+// create macros too
+
+trait QueryResult {
+    type Item;
+    fn next_item(&mut self) -> Option<Self::Item>;
+}
+
+struct ReadQueryResult<'a, T: Component> {
+    results: &'a [&'a [T]],
+    index_inner: usize,
+    index_outer: usize,
+}
+
+impl<'a, T: Component> QueryResult for ReadQueryResult<'a, T> {
+    type Item = &'a T;
+
+    fn next_item(&mut self) -> Option<Self::Item> {
+        if self.index_outer >= self.results.len() {
+            None
+        } else if self.index_inner >= self.results[self.index_outer].len() {
+            self.index_inner = 0;
+            self.index_outer += 1;
+            self.next_item()
+        } else {
+            let result = Some(&self.results[self.index_outer][self.index_inner]);
+            self.index_inner += 1;
+            result
+        }
+    }
+}
+
+impl<'a, T: Component> ReadQueryResult<'a, T> {
+    fn new(results: &'a [&'a [T]]) -> Self {
+        Self { results, index_inner: Default::default(), index_outer: Default::default() }
+    }
+}
+
+impl<'a, T: Component> Iterator for ReadQueryResult<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_item()
+    }
+}
+
+trait QueryIterator {
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+
+impl<A: QueryResult, B: QueryResult> QueryIterator for (A, B) {
+    type Item = (A::Item, B::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some((self.0.next_item()?, self.1.next_item()?))
+    }
+}
+
+
+// This is problematic. In order to define more tuples, more structs would need to be defined on each tuple combination.
+struct QueryResultTuple<A: QueryResult, B: QueryResult>((A, B));
+
+impl<A: QueryResult, B: QueryResult> Iterator for QueryResultTuple<A, B> {
+    type Item = (A::Item, B::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some((self.0.0.next_item()?, self.0.1.next_item()?))
+    }
+}
+
 
 pub trait Query
 {
@@ -31,6 +103,8 @@ pub struct FetchRead<T> (PhantomData<T>);
 impl<T: Component> Fetch for FetchRead<T> {
     type Item<'a> = &'a [T];
 
+    // this will need to change to take a reference to World
+    // and query across multiple tables
     fn fetch(table: &EntityTable) -> Self::Item<'_> {
         if table.has::<T>() {
             println!("table found with type {}", TypeInfo::of::<T>().type_name);
@@ -81,6 +155,7 @@ impl<'a, T: Component> Query for &'a T {
 
 pub struct QueryExecutor<'a, Q: Query> {
     tables: &'a Vec<EntityTable>,
+    data: Option<&'a [Q::Item<'a>]>,
     _marker: PhantomData<Q>,
 }
 
@@ -88,14 +163,31 @@ impl<'a, Q: Query> QueryExecutor<'a, Q> {
     pub fn new(tables: &'a Vec<EntityTable>) -> Self {
         Self {
             tables,
+            data: None,
             _marker: PhantomData::default(),
         }
     }
 
     pub fn execute(&self) -> <Q as Query>::Item<'_> {
         let fetcher = Q::Fetch::new();
+        // no logic to fetch the correct data from a range of queries yet
         let result = Q::get(&fetcher, &self.tables[0]);
         result
+    }
+    pub fn execute_on_self(&mut self) {
+        let fetcher = Q::Fetch::new();
+        // no logic to fetch the correct data from a range of queries yet
+        let result = Q::get(&fetcher, &self.tables[0]);
+        todo!()
+        // self.data = Some(result);
+    }
+}
+
+impl<'q, Q: Query> Iterator for QueryExecutor<'q, Q> {
+    type Item = Q::Item<'q>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
     }
 }
 
@@ -117,5 +209,32 @@ pub fn test() {
 
     for (a, b) in v {
         println!("{},{}", a, b)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::storage::query::{QueryResult, QueryResultTuple, ReadQueryResult};
+
+    #[test]
+    fn query_result_can_iter_over_nested() {
+        let result: &[&[i32]] = &[&[1, 2, 3, 4], &[5, 6, 7, 8], &[9, 10, 11, 12]];
+        let expected = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        let mut query_result = ReadQueryResult::new(result);
+        query_result.into_iter().zip(expected).for_each(|(a, b)| assert_eq!(a, b));
+    }
+
+    #[test]
+    fn query_result_can_iter_over_nested_tuple() {
+        let result: QueryResultTuple<ReadQueryResult<i32>, ReadQueryResult<i32>> =
+            QueryResultTuple(
+                (ReadQueryResult::new(&[&[1, 2, 3, 4], &[5, 6, 7, 8], &[9, 10, 11, 12]]),
+                 ReadQueryResult::new(&[&[1, 2, 3, 4], &[5, 6, 7, 8], &[9, 10, 11, 12]]))
+            );
+
+        for (a, b) in result {
+            println!("{}, {}", a, b)
+        }
     }
 }
