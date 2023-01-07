be606afe8947cd961a743a69603b2355518c956f
Harry Prior <harryprior@hotmail.co.uk>
2023-01-07 11:49:53 +0000

    tuples working for refs

--- src/storage/query.rs
+++ src/storage/query.rs
@@ -1,190 +1,3 @@
-// use crate::storage::column::Column;
-// use crate::storage::component::{Component, TypeInfo};
-// use crate::storage::table::EntityTable;
-// use std::any::TypeId;
-// use std::collections::{HashMap, HashSet};
-// use std::marker::PhantomData;
-//
-// // PROBLEM: It's not possible to alias structs with different generic type parameters. This current
-// // approach therefore is not possible
-//
-// // TODO: it would be nice to create a query interface that could return a tuple of arguments from a
-// // table in one iteration e.g. Query<'a, T, U> -> for (t, u) in query {}
-// //
-//
-// pub trait TQuery<'a> {
-//     type Item;
-//
-//     fn next_item(&mut self) -> Self::Item;
-//     fn to_trait(self) -> Box<dyn TQuery<'a, Item=Self::Item> + 'a>;
-// }
-//
-//
-// impl<'a, T> TQuery<'a> for Query<'a, T> {
-//     type Item = &'a T;
-//
-//     fn next_item(&mut self) -> Self::Item {
-//         self.next().unwrap()
-//     }
-//
-//     fn to_trait(self) -> Box<dyn TQuery<'a, Item=Self::Item> + 'a> {
-//         Box::new(self)
-//     }
-// }
-//
-// pub struct QueryTuple<'a, T, U>(Query<'a, T>, Query<'a, U>);
-//
-// impl<'a, T, U> TQuery<'a> for QueryTuple<'a, T, U> {
-//     type Item = (&'a T, &'a U);
-//
-//     fn next_item(&mut self) -> Self::Item {
-//         (self.0.next().unwrap(), self.1.next().unwrap())
-//     }
-//
-//     fn to_trait(self) -> Box<dyn TQuery<'a, Item=Self::Item> + 'a> {
-//         Box::new(self)
-//     }
-// }
-//
-// impl<'a, T, U> TQuery<'a> for (Query<'a, T>, Query<'a, U>) {
-//     type Item = (&'a T, &'a U);
-//
-//     fn next_item(&mut self) -> Self::Item {
-//         (self.0.next().unwrap(), self.1.next().unwrap())
-//     }
-//
-//     fn to_trait(self) -> Box<dyn TQuery<'a, Item=Self::Item> + 'a> {
-//         Box::new(self)
-//     }
-// }
-//
-//
-// pub fn use_query_interface<'a, Q, C, A>(tables: &'a Vec<EntityTable>) -> Q
-//     where Q: TQuery<'a, Item=(C, A)> + FromIterator<(&'a [C], &'a [A])>,
-//           C: Component,
-//           A: Component
-//
-// {
-//     let mut type_ids: HashSet<TypeId> = HashSet::new();
-//     let type_id1 = TypeInfo::of::<C>().id;
-//     type_ids.insert(type_id1);
-//
-//     let matching_tables: Vec<&EntityTable> =
-//         tables.iter().filter(|t| t.has_signature(&type_ids)).collect();
-//
-//     // generate queries
-//
-//     let mut query: Q =
-//         (&matching_tables)
-//             .iter()
-//             .map(|t| (t.get::<C>())).
-//             zip(
-//                 (&matching_tables)
-//                     .iter()
-//                     .map(|t| (t.get::<A>())))
-//             .collect();
-//
-//     query
-// }
-//
-// pub struct Query<'a, T> {
-//     data: Vec<&'a [T]>,
-//     outer_index: usize,
-//     inner_index: usize,
-// }
-//
-// impl<'a, T> Query<'a, T> {
-//     pub fn new() -> Self {
-//         Self {
-//             data: vec![],
-//             outer_index: 0,
-//             inner_index: 0,
-//         }
-//     }
-//
-//     pub fn push(&mut self, element: &'a [T]) {
-//         self.data.push(element);
-//     }
-// }
-//
-// impl<'a, T: Component> FromIterator<&'a [T]> for Query<'a, T> {
-//     fn from_iter<I: IntoIterator<Item=&'a [T]>>(iter: I) -> Self {
-//         let mut query = Query::new();
-//         for element in iter {
-//             query.push(element);
-//         }
-//         query
-//     }
-// }
-//
-// impl<'a, T: Component> FromIterator<&'a mut [T]> for Query<'a, T> {
-//     fn from_iter<I: IntoIterator<Item=&'a mut [T]>>(iter: I) -> Self {
-//         let mut query = Query::new();
-//         for element in iter {
-//             query.push(element);
-//         }
-//         query
-//     }
-// }
-//
-// impl<'a, T: Component, U: Component> FromIterator<(&'a [T], &'a [U])> for QueryTuple<'a, T, U> {
-//     fn from_iter<I: IntoIterator<Item=(&'a [T], &'a [U])>>(iter: I) -> Self {
-//         let mut query_one = Query::new();
-//         let mut query_two = Query::new();
-//         for element in iter {
-//             query_one.push(element.0);
-//             query_two.push(element.1);
-//         }
-//
-//         QueryTuple(query_one, query_two)
-//     }
-// }
-//
-// impl<'a, T> Iterator for Query<'a, T> {
-//     type Item = &'a T;
-//
-//     fn next(&mut self) -> Option<Self::Item> {
-//         let data = &self.data;
-//
-//         if self.outer_index >= data.len() {
-//             None
-//         } else if self.inner_index >= self.data[self.outer_index].len() {
-//             self.inner_index = 0;
-//             self.outer_index += 1;
-//             self.next()
-//         } else {
-//             let result = &data[self.outer_index][self.inner_index];
-//             self.inner_index += 1;
-//             Some(result)
-//         }
-//     }
-// }
-//
-// #[cfg(test)]
-// mod tests {
-//     use crate::storage::query::Query;
-//
-//     #[test]
-//     fn query_can_be_created_from_nested_iterable() {
-//         fn get_int_array<'a>() -> &'a [i32] {
-//             &[1, 2, 3, 4]
-//         }
-//         let query: Query<i32> = (0..3).into_iter().map(|a| get_int_array()).collect();
-//     }
-//
-//     #[test]
-//     fn query_can_be_iterated_over() {
-//         let query: Query<i32> = vec![&[0, 1, 2, 3] as &[i32], &[4, 5, 6, 7] as &[i32]]
-//             .into_iter()
-//             .collect();
-//
-//         for (i, elem) in query.enumerate() {
-//             assert_eq!(i, *elem as usize);
-//         }
-//     }
-// }
-//
-
 use std::marker::PhantomData;
 use crate::entity;
 use crate::storage::component::TypeInfo;
@@ -225,6 +38,28 @@ impl<T: Component> Fetch for FetchRead<T> {
     fn new() -> Self { Self { 0: Default::default() } }
 }
 
+impl<A: Fetch, B: Fetch> Fetch for (A, B) {
+    type Item<'a> = (A::Item<'a>, B::Item<'a>);
+
+    fn execute<'a>(table: &'a EntityTable) -> Self::Item<'a> {
+        (A::execute(&table), B::execute(&table))
+    }
+
+    fn new() -> Self {
+        (A::new(), B::new())
+    }
+}
+
+impl<A: Query, B: Query> Query for (A, B) {
+    type Item<'a> = (<<A as Query>::Fetch as Fetch>::Item<'a>, <<B as Query>::Fetch as Fetch>::Item<'a>);
+    type Fetch = (A::Fetch, B::Fetch);
+
+    fn get<'a>(fetch: &Self::Fetch, table: &'a EntityTable) -> Self::Item<'a> {
+        Self::Fetch::execute(table)
+    }
+}
+
+
 impl<'a, T: Component> Query for &'a T {
     type Item<'b> = &'b [T];
 
@@ -235,30 +70,6 @@ impl<'a, T: Component> Query for &'a T {
     }
 }
 
-// impl<A: Fetch, B:Fetch> Fetch for (A, B){
-//     type Item<'a> = (A::Item<'a>, B::Item<'a>);
-//
-//     // this constraint is going to require that I return a tuple of 
-//     fn execute<'a>(table: &EntityTable) -> &[Self::Item<'a>] {
-//         let result = (A::execute(&table), B::execute(table));
-//         result
-//
-//     }
-//
-//     fn new() -> Self {
-//         (A::new(), B::new())
-//     }
-// }
-//
-// impl<A: Query, B: Query> Query for (A, B){
-//     type Item<'q> = (A::Item<'q>, B::Item<'q>);
-//     type Fetch = (A::Fetch, B::Fetch);
-//
-//     fn get<'a>(fetch: &Self::Fetch, table: &'a EntityTable) -> &'a [Self::Item<'a>] {
-//         todo!()
-//     }
-// }
-//
 pub struct Start<Q: Query> {
     tables: Vec<EntityTable>,
     _marker: PhantomData<Q>,
@@ -272,7 +83,7 @@ impl<Q: Query> Start<Q> {
         }
     }
 
-    fn execute<'a>(&'a self) -> <Q as Query>::Item<'a> {
+    fn execute(&self) -> <Q as Query>::Item<'_> {
         let fetcher = Q::Fetch::new();
         let result = Q::get(&fetcher, &self.tables[0]);
         result
@@ -290,9 +101,12 @@ pub fn test() {
 
     let tables = vec![table];
 
-    let start: Start<&i32> = Start::new(tables);
+    let start: Start<(&i32, &f32)> = Start::new(tables);
     let data = start.execute();
-    for element in data {
-        println!("{}", element);
+
+    let v: Vec<(&i32, &f32)> = data.0.iter().zip(data.1.iter()).collect();
+
+    for (a, b) in v {
+        println!("{},{}", a, b)
     }
 }
